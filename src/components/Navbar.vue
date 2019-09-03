739e2e3f9f797cd6fd3b44f77a5f0e80554cc90e
Saugatmalla <saugat013@gmail.com>
2019-09-03 14:29:52 -0400

    Links fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,30 +2,21 @@
   <nav>
     <v-app-bar flat app>
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <router-link to="/">
       <v-toolbar-title class="text-uppercase grey--text">
         
         <span class="font-weight-light">App</span>
         <span>Weather</span>
-      
       </v-toolbar-title>
-      </router-link>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-list>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="white--text text-center" >
-              <router-link to="/"  class="white--text">
-              Home
-              </router-link>
+        <v-list-item v-for="link in links" :key="link" router :to="link.route">
+          <v-list-item-content >
+            <v-list-item-title class="white--text text-center" @click="drawer = !drawer">
+              {{link.name}}
             </v-list-item-title>
-            <v-list-item-title class="white--text text-center">
-              <router-link to="/weekly" class="white--text">
-              Weekly
-              </router-link>
-            </v-list-item-title >
+          
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -37,7 +28,11 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        {name: "Home", route: "/"},
+        {name: "Weekly", route: "/weekly"}
+      ]
     };
   }
 };
