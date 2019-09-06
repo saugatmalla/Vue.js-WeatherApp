94b6df0fdf1ec0ab19fd43be80e162f83d5a2b8b
Saugatmalla <saugat013@gmail.com>
2019-09-06 01:41:23 -0400

    Vuex added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
           placeholder="City, country"
         ></v-text-field>
 
-        <v-btn icon type="submit" @click="update">
+        <v-btn icon type="submit">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
       </v-toolbar>
@@ -19,32 +19,24 @@
 
     <!-- Weather Card -->
     <div class="card-wrapper" v-if="forecast.sys">
-      <v-card class="mx-auto" color="#F9F9F9" max-width="400" v-if="forecast">
+      <v-card class="mx-auto" color="#F9F9F9" max-width="400">
         <v-list-item two-line>
           <v-list-item-content>
-            <v-list-item-title
-              class="headline"
-            >{{forecast.name}} , {{forecast.sys.country}}
-            </v-list-item-title>
+            <v-list-item-title class="headline">{{forecast.name}} , {{forecast.sys.country}}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <!-- Current Temperature and Icon -->
         <v-card-text>
           <v-row align="center">
-            <v-col class="display-3" cols="6" >{{forecast.main.temp}}&deg;C</v-col>
-            <v-col
-              class="display-3"
-              cols="6"
-            >{{icons[forecast.weather[0].main]}}</v-col>
+            <v-col class="display-3" cols="6">{{forecast.main.temp}}&deg;C</v-col>
+            <v-col class="display-3" cols="6">{{icons[forecast.weather[0].main]}}</v-col>
           </v-row>
         </v-card-text>
 
         <!-- Temperature Min and Max -->
         <v-list-item>
-          <v-list-item-subtitle
-            
-          >{{forecast.main.temp_min}}&deg;C / {{forecast.main.temp_max}}&deg;C</v-list-item-subtitle>
+          <v-list-item-subtitle>{{forecast.main.temp_min}}&deg;C / {{forecast.main.temp_max}}&deg;C</v-list-item-subtitle>
         </v-list-item>
 
         <v-list-item>
@@ -55,86 +47,63 @@
         </v-list-item>
 
         <v-list-item>
-          <v-list-item-icon >
+          <v-list-item-icon>
             <v-icon>mdi-send</v-icon>
           </v-list-item-icon>
           <v-list-item-subtitle>{{forecast.wind.speed}}km/hr</v-list-item-subtitle>
         </v-list-item>
 
         <v-list-item>
-          <v-list-item-icon >
+          <v-list-item-icon>
             <v-icon>mdi-cloud-download</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle >{{forecast.clouds.all}}%</v-list-item-subtitle>
+          <v-list-item-subtitle>{{forecast.clouds.all}}%</v-list-item-subtitle>
         </v-list-item>
       </v-card>
     </div>
-    <div class="card-wrapper" v-else-if="this.forecast.cod">
-      <p class="text-capitalize blockquote">
-        {{this.errors}} or Wrong Format
-        <br />
-        <span class="subtitle-1">Try searching in this format (City, country)</span>
-      </p>
-    </div>
+
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import axios from "axios";
+
 export default {
   name: "home",
   data() {
     return {
-      time: 0,
-      forecast: "",
       location: "",
-      city: "Toronto,ca",
-      country: "ca",
-      // Icons
-      icons: {
-        Clear: "😐",
-        Rain: "🌧️",
-        Fog: "🌫️",
-        Snow: "⛄️",
-        Wind: "💨",
-        Clouds: "☁️",
-        Haze: "🌫️",
-        Mist: "🌈",
-        Rainbow: "🌈",
-        Sunny: "☀️",
-        Night: "🌃"
-      },
-      errors: {}
+      errors: '',
     };
   },
+
+  computed: {
+    ...mapState(["forecast", "icons"])
+  },
   mounted() {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        this.city +
-        "&appid=37c74120d05b58e1807a5286598c9d90&units=metric"
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.forecast = data;
-      });
+    this.$store.dispatch("getForecast");
   },
 
   methods: {
     update: function() {
-      this.city = this.location;
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          this.city +
-          "&appid=37c74120d05b58e1807a5286598c9d90&units=metric"
-      )
-        .then(response => response.json())
+      axios
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?q=" +
+            this.location +
+            "&appid=37c74120d05b58e1807a5286598c9d90&units=metric"
+        )
         .then(data => {
-          this.forecast = data;
+          this.$store.state.forecast = data.data;
           this.location = "";
-
-          if (this.forecast.cod == "404") {
-            this.errors = this.forecast.message;
-          } else {
-            return this.forecast;
+          
+        })
+        .catch(error => {
+          if ((error = "Request failed with status code 404")) {
+            this.errors = "Wrong City Name";
+            alert(this.errors)
+            
+            
           }
         });
     }
